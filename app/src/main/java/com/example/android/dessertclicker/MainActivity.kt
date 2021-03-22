bbc6ebe7c6db265d6bb44be7b0b97e254eae2884
Bian Taufikurrahman Atmaja <bianatmaja@student.telkomuniversity.ac.id>
2021-03-22 22:24:17 +0700

    Menggunakan Timer pada Lifecycle

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dessertTimer: DessertTimer
 
     /** Dessert Data **/
 
@@ -76,6 +77,8 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+        dessertTimer = DessertTimer()
+
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -89,6 +92,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         //Log.i("MainActivity", "onStart Called")
         Timber.i("onCreate called")
+        dessertTimer.startTimer()
 
     }
     override fun onResume() {
@@ -104,6 +108,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop Called")
+        dessertTimer.stopTimer()
     }
 
     override fun onDestroy() {
